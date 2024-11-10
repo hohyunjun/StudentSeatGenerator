98cce4ccd14d283bdbababe18a443bfb4c23bb5c
jhh5154@naver.com <hohyun@Junui-MacBookAir.local>
2024-11-11 00:00:19 +0900

    deploy testing...

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://hohyunjun.github.io/StudentSeatGenerator"
-      : "",
+  output: "export",
+  assetPrefix: "https://hohyunjun.github.io/StudentSeatGenerator",
 };
 
 export default nextConfig;
